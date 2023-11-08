23d8ee100e2b7540d229538462a46a50849f2ba9
zeeb <zeeb1337@hotmail.com>
2023-11-08 07:16:01 +0100

    Update definitions.h

--- src/definitions.h
+++ src/definitions.h
@@ -5,8 +5,8 @@
 #define FS_DEFINITIONS_H_877452FEC245450C9F96B8FD268D8963
 
 static constexpr auto STATUS_SERVER_NAME = "The Forgotten Server";
-static constexpr auto STATUS_SERVER_VERSION = "1.4.2";
-static constexpr auto STATUS_SERVER_DEVELOPERS = "The Forgotten Server Team";
+static constexpr auto STATUS_SERVER_VERSION = "1.41";
+static constexpr auto STATUS_SERVER_DEVELOPERS = "The Forgotten Server Team & zeeb1337";
 
 static constexpr auto CLIENT_VERSION_MIN = 1097;
 static constexpr auto CLIENT_VERSION_MAX = 1098;
